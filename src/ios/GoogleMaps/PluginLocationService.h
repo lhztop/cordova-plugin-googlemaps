b5b906651f7924f7fbb81e78b2f76a48fa2c99ee
lhz <lhztop@163.com>
2021-08-01 13:53:03 +0800

    ios location

--- src/ios/GoogleMaps/PluginLocationService.h
+++ src/ios/GoogleMaps/PluginLocationService.h
@@ -7,14 +7,17 @@
 //
 
 #import <Cordova/CDV.h>
-#import <GoogleMaps/GoogleMaps.h>
+#import <AMapLocationKit/AMapLocationKit.h>
+#import <AMapSearchKit/AMapSearchKit.h>
 #import "PluginUtil.h"
 
-@interface PluginLocationService : CDVPlugin<CLLocationManagerDelegate>
+@interface PluginLocationService : CDVPlugin<AMapLocationManagerDelegate, AMapSearchDelegate>
 
 @property (nonatomic, strong) NSMutableDictionary *lastResult;
 @property (nonatomic, strong) CLLocation *lastLocation;
-@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) CLLocationManager *nativeLocationManager;
+@property (nonatomic, strong) AMapLocationManager *locationManager;
+@property (nonatomic, strong) AMapSearchAPI *searchApi;
 @property (nonatomic, strong) NSMutableArray *locationCommandQueue;
 - (void)getMyLocation:(CDVInvokedUrlCommand*)command;
 - (void)hasPermission:(CDVInvokedUrlCommand*)command;
